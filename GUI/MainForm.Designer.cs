2892e49ebf8350b9938d07dfb0b1d4f880f6918a
Hiep <vuxuanhiep1709@gmail.com>
2023-10-30 23:31:38 +0700

    change

--- GUI/MainForm.Designer.cs
+++ GUI/MainForm.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(86, 42);
+            this.label1.Location = new System.Drawing.Point(534, 105);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(139, 18);
             this.label1.TabIndex = 0;
